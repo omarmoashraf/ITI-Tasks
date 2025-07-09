02377db7e2c68a9ef536ea304e3468cfdd0d009d
omarmoashraf <omarmoashraf05@gmail.com>
2025-07-09 13:53:00 +0300

    Max Grade

---
+++ ITI-Day2/ITI_task3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int Max = -1;
+            int[] Degrees = new int[n];
+            for (int i = 0; i < Degrees.Length; i++)
+            {
+                int grade = int.Parse(Console.ReadLine());
+                Degrees[i] = grade;
+                if (grade > Max)
+                {
+                    Max = grade;
+                }
+            }
+            Console.WriteLine($"The maximum grade is {Max}");
+
+        }
+    }
+}
